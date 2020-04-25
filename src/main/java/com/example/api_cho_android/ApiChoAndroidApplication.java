b5a2d6e25de03f47537ffe2b32e0a09e64030103
KimThaoUitk12 <51397805+KimThaoUitk12@users.noreply.github.com>
2020-04-26 01:01:26 +0700

    first commit

---
+++ src/main/java/com/example/api_cho_android/ApiChoAndroidApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_cho_android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChoAndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiChoAndroidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_cho_android/ApiChoAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_cho_android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChoAndroidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
