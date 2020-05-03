45040eb6bcb2d18c759c97dec4f0b746e86590f3
Phamhuynhmyhanh <64317817+Phamhuynhmyhanh@users.noreply.github.com>
2020-05-03 14:06:23 +0700

    Revert "Thêm vào Package Dto 4 file NguoiDungDto, PlayListDto,BaiHat_PlayListDto,DownloasDto Thêm vào Package Converter 4 file  NguoiDungConverter , PlayListConverter , BaiHat_PlayListConverter ,DownloasConverter. Chỉnh sửa entity, repository,service, controller. Hết lỗi . Đã test thành công"
    This reverts commit 87b8845ded5d0c33cfbe00fe0ce78ef4805cd3e0.

--- service/DownloadService.java
+++
@@ -1,9 +0,0 @@
-package com.example.api_cho_android.service;
-
-import com.example.api_cho_android.model.Download;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface DownloadService {
-	public Download addDownload(Download download);
-}
