256efde1ace7759a9c78c6a2df3471c2b3a49ac2
KimThaoUitk12 <51397805+KimThaoUitk12@users.noreply.github.com>
2020-05-09 19:16:20 +0700

    Thêm dependencies, làm SecurityConfig.java

---
+++ src/main/java/com/example/api_cho_android/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.api_cho_android.config;
+
+import com.example.api_cho_android.service.NguoiDungService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private NguoiDungService nguoiDungService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("select email, pass, true from nguoidung where email=?")
+                .authoritiesByUsernameQuery("select email, true from nguoidung where email=? ");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().hasAnyRole().and().csrf().disable().httpBasic();
+    }
+}
