a468734e7183548938de882e4c6fef0784ab283c
Phamhuynhmyhanh <64317817+Phamhuynhmyhanh@users.noreply.github.com>
2020-05-12 14:59:32 +0700

    Revert "Add files via upload"
    This reverts commit 573e0d4a40459bca1010a1a2b5f22a9c314bfa64.

--- src/main/java/com/example/api_cho_android/config/SecurityConfig.java
+++ src/main/java/com/example/api_cho_android/config/SecurityConfig.java
@@ -2,13 +2,11 @@ package com.example.api_cho_android.config;
 
 import com.example.api_cho_android.service.NguoiDungService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -25,16 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("select email, pass, true from nguoidung where email=?")
                 .authoritiesByUsernameQuery("select email, true from nguoidung where email=? ");
-        
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated().and().csrf().disable().httpBasic();
-    }
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+                .anyRequest().hasAnyRole().and().csrf().disable().httpBasic();
     }
 }
